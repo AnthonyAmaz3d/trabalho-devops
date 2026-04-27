867a97da00fca070954c566685e81717faf7e913
AnthonyAmaz3d <anthony.amaz3d@gmail.com>
2026-04-27 13:25:44 -0300

    tinha commitado apenas o ci-cd

---
+++ test_app.py
@@ -0,0 +1,24 @@
+from app import app
+
+def test_app_exists():
+    assert app is not None
+
+def test_home_status_code():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+
+def test_home_data():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert b'Professor' in response.data
+
+def test_404_page():
+    tester = app.test_client()
+    response = tester.get('/pagina-que-nao-existe')
+    assert response.status_code == 404
+
+def test_method_not_allowed():
+    tester = app.test_client()
+    response = tester.post('/')
+    assert response.status_code == 405
